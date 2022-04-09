9768cd9b1a2a2c8249485690eebaaea34992161b
Callum Bloys <callum2.bloys@live.uwe.ac.uk>
2022-04-09 21:12:44 +0100

    fixed counter

--- AdvancedTechProject2/Assets/Scripts/AIPlayerControls.cs
+++ AdvancedTechProject2/Assets/Scripts/AIPlayerControls.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class AIPlayerControls : MonoBehaviour
 {
@@ -13,9 +14,13 @@ public class AIPlayerControls : MonoBehaviour
     private bool destinationmissing = false;
     public static int current_destination;
 
+    [SerializeField] private Text destinationText;
+
     void Start()
     {
         navmeshagent = GetComponent<NavMeshAgent>();
+
+        destinationText.text = "Current Destination: 0/11";
     }
 
     void Update()
@@ -42,6 +47,11 @@ public class AIPlayerControls : MonoBehaviour
         if (destinationmissing)
         {
             current_destination++;
-        }      
+        }
+
+        if(current_destination <= 11)
+        {
+            destinationText.text = "Current Destination: " + current_destination + "/11";
+        }
     }
 }
